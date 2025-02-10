08bc89a3ce5c1a33c272bbf0e3fe01fa5f2b87d4
rashodk <rmanamperiko@mun.ca>
2025-02-10 15:57:05 -0330

    Refactor authentication pages to use dedicated form components; add layout for authentication flow

---
+++ app/(Auth)/layout.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import type { Metadata } from "next";
+import Image from "next/image";
+
+export const metadata: Metadata = {
+    title: "Aether",
+    description: "Bringing Authenticity to Creativity",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="grid min-h-svh lg:grid-cols-2">
+            <div className="flex flex-col gap-4 p-6 md:p-10">
+                <div className="flex justify-center gap-2 md:justify-start">
+                    <a href="#" className="flex items-center gap-2 font-medium">
+                        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                            Æ
+                        </div>
+                        Aether Inc.
+                    </a>
+                </div>
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="w-full max-w-xs">
+                        {children}
+                    </div>
+                </div>
+            </div>
+            <div className="relative hidden bg-muted lg:block">
+                <Image
+                    src="/aetherhero1.png"
+                    alt="Image"
+                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                    width={1000}
+                    height={1000}
+                />
+            </div>
+        </div>
+    );
+}
