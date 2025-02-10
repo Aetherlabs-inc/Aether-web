7b7db88af736fd683ebb629f3e8ed10667099af6
rashodk <rmanamperiko@mun.ca>
2025-02-10 15:57:05 -0330

    Refactor authentication pages to use dedicated form components; add layout for authentication flow

--- app/(Auth)/signup/page.tsx
+++ app/(Auth)/signup/page.tsx
@@ -1,12 +1,10 @@
 import React from "react"
-import { LoginForm } from "@/src/auth/login"
+import { SignupForm } from "@/src/auth/signup/signup-form"
 
-export default function LoginPage() {
+export default function SignupPage() {
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-            <div className="w-full max-w-sm md:max-w-3xl">
-                <LoginForm />
-            </div>
-        </div>
+        <>
+            <SignupForm />
+        </>
     )
 }
